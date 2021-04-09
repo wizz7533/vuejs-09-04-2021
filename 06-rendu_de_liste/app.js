ae41da31c208f51f349273fbb9692462f7bdcfca
ylesueur-dawan <ylesueur@dawan.fr>
2021-04-09 15:17:37 +0200

    Le rendu de listE

--- 06-rendu_de_liste/app.js
+++ 06-rendu_de_liste/app.js
@@ -1,6 +1,11 @@
 let app = Vue.createApp({
     data() {
         return {
+            prenoms: ['Rick', 'Beth', 'Jerry', 'Summer'],
+            utilisateurs: [
+                {nom: 'Doe', prenom: 'John'},
+                {nom: 'Zarella', prenom: 'Maude'}
+            ]
         }
     }
 });
