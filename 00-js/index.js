26b1eb91c96ffd6cdfdcff239874a7985bd38f22
ylesueur-dawan <ylesueur@dawan.fr>
2021-04-09 09:33:26 +0200

    Javascript

---
+++ 00-js/index.js
@@ -0,0 +1,72 @@
+let prenom = "David";
+let nom = 'Doe';
+let adresse = `3ter 'rue' "d'arsonval", ${75015}`;
+
+console.log(prenom);
+console.log(nom);
+console.log(adresse);
+
+const tableau = [1,5,10];
+
+let [a, b, reste] = tableau;
+[a, b, ...reste] = tableau;
+
+
+let voiture = {
+    couleur: "bleu"
+};
+
+console.log("Couleur de la voiture: " + voiture.couleur);
+
+let tableau2 = tableau;
+tableau2.push('Yohann');
+console.log(tableau);
+
+// Copier un tableau
+tableau2 = [...tableau];
+// Copier un objet
+const voiture2 = {...voiture};
+console.log(voiture2);
+voiture2.couleur = "Rouge"
+console.log("voiture2: ", voiture2);
+console.log("voiture: ", voiture);
+
+
+let nombres = [2,7,9,9];
+for (const nombre of nombres) {
+    console.log("Nombre: " + nombre);
+}
+
+console.log("_______ Fonction anonymes _______");
+
+function afficherNom(prenom) {
+    console.log("Le prenom: " + prenom);
+}
+
+function queDoisJeFaireDeCePrenom(myCallBack) {
+    prenom = "Maude";
+    myCallBack(prenom);
+}
+
+queDoisJeFaireDeCePrenom(afficherNom);
+
+// Fonction anonyme
+queDoisJeFaireDeCePrenom(function(prenom) {
+    console.log("Le prenom: " + prenom.toUpperCase());
+})
+
+// Fonction flechée
+queDoisJeFaireDeCePrenom((prenom) => {
+    console.log("Le prenom: " + prenom.toUpperCase());
+})
+queDoisJeFaireDeCePrenom(prenom => console.log("Le prenom: " + prenom.toUpperCase()))
+
+voiture = {
+    couleur: 'rouge',
+    freiner: function() {
+
+    },
+    accelerer() {
+
+    }
+}
