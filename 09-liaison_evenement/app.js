7f2d68afee7c3faefa0eb799086fdcf602d61d81
ylesueur-dawan <ylesueur@dawan.fr>
2021-04-12 10:13:07 +0200

    Event

---
+++ 09-liaison_evenement/app.js
@@ -0,0 +1,12 @@
+let app = Vue.createApp({
+    data() {
+        return {
+        }
+    },
+    methods: {
+        sayHello() {
+            console.log("Hello !");
+        }
+    }
+});
+app.mount('#app');
