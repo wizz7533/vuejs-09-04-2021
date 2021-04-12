fdc43fb693bb8be3654e41052e85c85479ee6a8e
ylesueur-dawan <ylesueur@dawan.fr>
2021-04-12 10:36:13 +0200

    Event

--- 09-liaison_evenement/app.js
+++ 09-liaison_evenement/app.js
@@ -1,11 +1,23 @@
 let app = Vue.createApp({
     data() {
         return {
+            prenom: "Ella"
         }
     },
     methods: {
-        sayHello() {
-            console.log("Hello !");
+        sayHello(message) {
+            console.log(message);
+        },
+
+        changerPrenom() {
+            // Pour acceder aux variables dans data() {}
+            // Il faut utiliser le mot clef this.
+            this.prenom = "Sarah";
+        },
+
+        afficherValeur(evenement){
+            // console.log(evenement);
+            console.log(evenement.target.value);
         }
     }
 });
