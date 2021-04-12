ba04b8527aa18e45d28de43e80a42fee3e9ad004
ylesueur-dawan <ylesueur@dawan.fr>
2021-04-12 17:06:31 +0200

    Exercices

--- 11-exercices/02-todolist/app2.js
+++ 11-exercices/02-todolist/app2.js
@@ -1,16 +1,33 @@
 let app = Vue.createApp({
     data() {
         return {
-            todos: [
-                {}
-            ]
+            todos: [],
+            options: [
+                { label: 'Urgent', type: 'alert alert-danger'},
+                { label: 'Important', type: 'alert alert-warning'},
+                { label: 'Pas important', type: 'alert alert-success'}
+            ],
         }
     },
     methods: {
         addTask(event) {
-            event.preventDefault();
             let texte = event.target.task.value;
-            this.todos.push(texte);
+            let taille = event.target.size.value;
+            
+            // console.log("Texte:" + texte);
+            // console.log("Taille: " + taille);
+            // console.log("Couleur:" + event.target.level.value);
+
+            let task = {
+                texte: texte,
+                style: {
+                    fontSize: `${taille}px`,
+                    listStyle: 'none'
+                },
+                couleur: event.target.level.value
+            }
+
+            this.todos.push(task);
         }
     }
 });
