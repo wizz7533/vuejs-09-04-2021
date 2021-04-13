ffca023b21aab9d0900f155114c4586af15411c0
ylesueur-dawan <ylesueur@dawan.fr>
2021-04-13 11:55:13 +0200

    Les composants

--- 14-composant_event/app.js
+++ 14-composant_event/app.js
@@ -5,6 +5,10 @@ let app = Vue.createApp({
         }
     },
     methods: {
+        incrementerParent(event) {
+            console.log("Event: ", event);
+            console.log("Type of Event: ", typeof event);
+        }
     },
     computed: {
     },
@@ -16,11 +20,30 @@ let app = Vue.createApp({
 app.component('v-card', {
     name: 'Card',
     // La propriete msg fait desormais partie des variables du composant footer
-    props: [],
+    props: ['compteur'],
+    // On ne doit pas modifier la props, elle est en lecture seule
+    // donc on doit la copier avant de l'utiliser dans le composant enfant
+    data() {
+        return {
+            childCount: this.compteur
+        }
+    },
+    methods: {
+        incrementer() {
+            this.childCount++;
+            // Declenchement d'un evenement personnalisé 
+            // la fonction $emit prend 2 arguments:
+            //      1- le nom de l'evenement (je peux mettre ce que je veux comme nom)
+            //      2- la valeur/variable/objet... a transmettre
+            this.$emit('my-incrementer-compteur', 5);
+        }
+    },
     template: `
         <div>
-            <p>Compteur Enfant: {{ count }}</p>
-            <button>Incrementer</button>
+            <p>Compteur Enfant: {{ childCount }}</p>
+            <button @click="incrementer()">
+                Incrementer
+            </button>
         </div>
     `
 });
