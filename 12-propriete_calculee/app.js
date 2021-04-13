70c9a2f52e97290307749c3ac43cf6c8b2be0f9b
ylesueur-dawan <ylesueur@dawan.fr>
2021-04-13 10:02:07 +0200

    Exercices

--- 12-propriete_calculee/app.js
+++ 12-propriete_calculee/app.js
@@ -16,16 +16,21 @@ let app = Vue.createApp({
         // Exécuter une fois au lancement de l'application, puis le resultat est mis en cache. cette fonction ne sera rappellé seulement si la variable this.message change.
         reverseMessageComputed() {
             console.count('ReverseMessageComputed');
+            // message = this.reverseMessage();
+            // console.log(message);
             return this.message.split('').reverse().join('');
         }
     },
     watch: {
-        // Traitement Async
+        // Généralement utilisé pour les traitements asynchrones
         titre(newValeur, oldValeur) {
             console.log("Watch");
             console.log("Vouvelle Valeur: ",newValeur );
             console.log("Ancienne Valeur: ", oldValeur);
         }
+    },
+    created: function() {
+        console.log("Creation de l'application");
     }
 });
 app.mount('#app');
