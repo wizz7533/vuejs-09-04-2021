a503d684c5b8439a8b9bec5829f6c73acae94d40
ylesueur-dawan <ylesueur@dawan.fr>
2021-04-13 09:49:54 +0200

    Les proprietes calculees

--- 12-propriete_calculee/app.js
+++ 12-propriete_calculee/app.js
@@ -1,12 +1,30 @@
 let app = Vue.createApp({
     data() {
         return {
-            message: 'Salut !'
+            message: 'Salut !',
+            rienAVoir: '',
+            titre: ''
         }
     },
     methods: {
         reverseMessage() {
+            console.count('ReverseMessage');
             return this.message.split('').reverse().join('');
+        }
+    },
+    computed: {
+        // Exécuter une fois au lancement de l'application, puis le resultat est mis en cache. cette fonction ne sera rappellé seulement si la variable this.message change.
+        reverseMessageComputed() {
+            console.count('ReverseMessageComputed');
+            return this.message.split('').reverse().join('');
+        }
+    },
+    watch: {
+        // Traitement Async
+        titre(newValeur, oldValeur) {
+            console.log("Watch");
+            console.log("Vouvelle Valeur: ",newValeur );
+            console.log("Ancienne Valeur: ", oldValeur);
         }
     }
 });
