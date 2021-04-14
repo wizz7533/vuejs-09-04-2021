0df2f031d333a607204e36199c0c2b0875fe7a38
ylesueur-dawan <ylesueur@dawan.fr>
2021-04-14 17:02:49 +0200

    Vuex

---
+++ 18-vuex/src/store/index.js
@@ -0,0 +1,19 @@
+import { createStore } from 'vuex'
+
+export default createStore({
+  // Equivalent à Data
+  state: {
+  },
+  // Responsable du changement d'etat de l'application
+  mutations: {
+  },
+  // Equivalent aux Méthods (Traitement complexe ou asyncrhone, puis appel la mutation)
+  actions: {
+  },
+  //Equivalent des proriétés calculée
+  getters: {},
+
+  // Equivalent a components: {}
+  modules: {
+  }
+});
