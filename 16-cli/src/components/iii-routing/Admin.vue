c26d3ef54eaba4c6d94004ebed8cc986fdcbde82
ylesueur-dawan <ylesueur@dawan.fr>
2021-04-15 16:41:59 +0200

    Login

--- 16-cli/src/components/iii-routing/Admin.vue
+++ 16-cli/src/components/iii-routing/Admin.vue
@@ -1,12 +1,19 @@
 <template>
     <div>
         <h1>Page d'administration</h1>
+        <button @click="deco">Deconnexion</button>
     </div>
 </template>
 
 <script>
+    import userStore from '@/store/user';
     export default {
-        
+        methods: {
+            deco() {
+                userStore.logout();
+                this.$router.push('/login');
+            }
+        }        
     }
 </script>
 
